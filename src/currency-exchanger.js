ae9e6e4f30c31c75b4249741e9f659cb35880241
NikkitaTorres <nikkitatorres@yahoo.com>
2023-11-17 17:05:41 -0800

    remove api key from URL

--- src/currency-exchanger.js
+++ src/currency-exchanger.js
@@ -1,7 +1,7 @@
 //BUSINESS/SERVICE LOGIC
 export default class ExchangeService {  
   static getCurrency(placeHolder) {
-    return fetch(`https://v6.exchangerate-api.com/v6/26f85f40cf2d0be6f6a54166/latest/USD`)
+    return fetch(`https://v6.exchangerate-api.com/v6/${API_KEY}/latest/USD`)
       .then(function(response) {
         if (!response.ok) {
           const errorMessage = `${response.status} ${response.statusText}`;
